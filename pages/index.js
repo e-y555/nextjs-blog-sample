1c27f1e2a61fb5fa2431657aba9f92618a6ebf26
e-y555 <e.y555acm.uw@gmail.com>
2023-01-29 00:18:41 +0900

    formatter

--- pages/index.js
+++ pages/index.js
@@ -10,66 +10,78 @@ import utilStyle from '../styles/utils.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-    return (
-        <Layout>
-            <section className={utilStyle.headingMd}>
-                <p>
-                    プロフィール紹介文が入ります。プロフィール紹介文が入ります。プロフィール紹介文が入ります。
-                </p>
-            </section>
-            <section>
-                <h2>📝エンジニアのブログ</h2>
-                <div className={styles.grid}>
-                    <article>
-                        <Link href='/'>
-                            <img src='/images/thumbnail01.jpg' className={styles.thumbnailImage} />
-                        </Link>
-                        <Link legacyBehavior href='/'>
-                            <a className={utilStyle.boldText}>
-                                タイトルが入ります。タイトルが入ります。タイトルが入ります。
-                            </a>
-                        </Link>
-                        <br />
-                        <small className={utilStyle.lightText}>February 23, 2020</small>
-                    </article>
-                    <article>
-                        <Link href='/'>
-                            <img src='/images/thumbnail01.jpg' className={styles.thumbnailImage} />
-                        </Link>
-                        <Link legacyBehavior href='/'>
-                            <a className={utilStyle.boldText}>
-                                タイトルが入ります。タイトルが入ります。タイトルが入ります。
-                            </a>
-                        </Link>
-                        <br />
-                        <small className={utilStyle.lightText}>February 23, 2020</small>
-                    </article>
-                    <article>
-                        <Link href='/'>
-                            <img src='/images/thumbnail01.jpg' className={styles.thumbnailImage} />
-                        </Link>
-                        <Link legacyBehavior href='/'>
-                            <a className={utilStyle.boldText}>
-                                タイトルが入ります。タイトルが入ります。タイトルが入ります。
-                            </a>
-                        </Link>
-                        <br />
-                        <small className={utilStyle.lightText}>February 23, 2020</small>
-                    </article>
-                    <article>
-                        <Link href='/'>
-                            <img src='/images/thumbnail01.jpg' className={styles.thumbnailImage} />
-                        </Link>
-                        <Link legacyBehavior href='/'>
-                            <a className={utilStyle.boldText}>
-                                タイトルが入ります。タイトルが入ります。タイトルが入ります。
-                            </a>
-                        </Link>
-                        <br />
-                        <small className={utilStyle.lightText}>February 23, 2020</small>
-                    </article>
-                </div>
-            </section>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>
+          プロフィール紹介文が入ります。プロフィール紹介文が入ります。プロフィール紹介文が入ります。
+        </p>
+      </section>
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href='/'>
+              <img
+                src='/images/thumbnail01.jpg'
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href='/'>
+              <a className={utilStyle.boldText}>
+                タイトルが入ります。タイトルが入ります。タイトルが入ります。
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img
+                src='/images/thumbnail01.jpg'
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href='/'>
+              <a className={utilStyle.boldText}>
+                タイトルが入ります。タイトルが入ります。タイトルが入ります。
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img
+                src='/images/thumbnail01.jpg'
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href='/'>
+              <a className={utilStyle.boldText}>
+                タイトルが入ります。タイトルが入ります。タイトルが入ります。
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img
+                src='/images/thumbnail01.jpg'
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link legacyBehavior href='/'>
+              <a className={utilStyle.boldText}>
+                タイトルが入ります。タイトルが入ります。タイトルが入ります。
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
